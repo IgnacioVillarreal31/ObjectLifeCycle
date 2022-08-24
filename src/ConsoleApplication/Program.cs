051220f8c6d727b68736666ac9912ac5dcf0de7f
IgnacioVillarreal31 <IGNACIO.VILLARREAL@correo.ucu.edu.uy>
2022-08-24 11:19:45 -0300

    actualizacion 2

--- src/ConsoleApplication/Program.cs
+++ src/ConsoleApplication/Program.cs
@@ -19,10 +19,16 @@ namespace ConsoleApplication
         /// </summary>
         public static void Main()
         {
+            for (int i=0;i<100000;i++)
+            {
+                new Train("Prueba");
+            }
+            // Lo que hace el lenguaje es borrar los objetos que no se usan cada cierto valor para liberar espacio en la memoria, entonces cuando creas muchas instancias de clase como 100000 lo que hace c# es borrar algunas de estas.
             Train t1 = new Train("Last Train To London");
             Train t2 = new Train("Last Train To London");
             Train t3 = new Train("Runaway Train");
             Console.WriteLine($"t1==t2 {t1==t2},t2==t3 {t2==t3}");
+        // Lo que ocurre es que t1, t2 y t3 son instancias de la clase distintas por lo que ocupan diferentes espacios en la memoria, aunque se pasen como parametro los mismos valotes, no son los mismos objetos
         }
     }
 }
